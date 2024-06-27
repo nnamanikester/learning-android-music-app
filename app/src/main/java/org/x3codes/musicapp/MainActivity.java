2c3d26c050dba291ff70bb6698d5da19b0943bfc
John Kester <nnamanikester@gmail.com>
2024-06-27 19:27:56 +0100

    feat: created a MusicService that plays in the background

--- app/src/main/java/org/x3codes/musicapp/MainActivity.java
+++ app/src/main/java/org/x3codes/musicapp/MainActivity.java
@@ -1,6 +1,10 @@
 package org.x3codes.musicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+    Button startBtn, stopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txt = findViewById(R.id.textView);
+        startBtn = findViewById(R.id.start_btn);
+        stopBtn = findViewById((R.id.stop_btn));
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent serviceIntent = new Intent(getApplicationContext(), MusicService.class);
+                startService(serviceIntent);
+            }
+        });
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent serviceIntent = new Intent(getApplicationContext(), MusicService.class);
+                stopService(serviceIntent);
+            }
+        });
+
     }
 }
